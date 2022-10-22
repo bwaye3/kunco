0f4c32f27507cfa2aa886501759c29ed9d98220e
Brad Waye <bradwaye@comm-bwaye3.local>
2022-10-22 12:03:45 -0400

    vendor twig mod

---
+++ web/themes/gavias_kunco/vendor/twig/twig/src/Loader/ChainLoader.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of Twig.
+ *
+ * (c) Fabien Potencier
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Twig\Loader;
+
+use Twig\Error\LoaderError;
+
+/**
+ * Loads templates from other loaders.
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ */
